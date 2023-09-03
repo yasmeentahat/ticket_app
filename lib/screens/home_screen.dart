2c70a5f416982181aec7f4fdcf2a16f12516c325
yasmeentahat <yasmeentahat02@gmail.com>
2023-09-03 19:09:48 +0300

    First Committ

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,108 @@
+import 'package:fluentui_icons/fluentui_icons.dart';
+import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:ticket_app/screens/ticket_view.dart';
+
+import '../utils/app_info_list.dart';
+import '../utils/app_styles.dart';
+import '../widgets/title_with_action_text.dart';
+import 'hotel_screen.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Styles.bgColor,
+      body: ListView(
+        children: [
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              children: [
+                const Gap(40),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Good morning",
+                          style: Styles.headlineStyle3,
+                        ),
+                        const Gap(5),
+                        Text(
+                          "Book Tickets",
+                          style: Styles.headlineStyle1,
+                        ),
+                      ],
+                    ),
+                    Container(
+                      height: 50,
+                      width: 50,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          image: const DecorationImage(
+                              fit: BoxFit.cover,
+                              image: AssetImage("assets/images/img_1.png"))),
+                    )
+                  ],
+                ),
+                const Gap(25),
+                Container(
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: const Color(0xFFF4F6FD)),
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 12, vertical: 12),
+                  child: Row(
+                    children: [
+                      const Icon(
+                        FluentSystemIcons.ic_fluent_search_regular,
+                        color: Color(0xFFBFC205),
+                      ),
+                      const SizedBox(
+                        width: 6,
+                      ),
+                      Text(
+                        "Search",
+                        style: Styles.headlineStyle4,
+                      )
+                    ],
+                  ),
+                ),
+                const Gap(40),
+                const AppTitleWithActionText(
+                    title: "Upcoming flights", actionText: "View all"),
+              ],
+            ),
+          ),
+          const Gap(15),
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            padding: const EdgeInsets.only(left: 20),
+            child: Row(
+              children:
+                  ticketList.map((item) => TicketView(ticket: item)).toList(),
+            ),
+          ),
+          const Gap(15),
+          Container(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: const AppTitleWithActionText(
+                  title: "Hotels", actionText: "View all")),
+          const Gap(15),
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            padding: const EdgeInsets.only(left: 20),
+            child: Row(
+                children:
+                    hotelList.map((item) => HotelScreen(hotel: item)).toList()),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/screens/ticket_view.dart
@@ -0,0 +1,205 @@
+import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:get/get.dart';
+
+import '../utils/app_layout.dart';
+import '../utils/app_styles.dart';
+import '../widgets/column_layout.dart';
+import '../widgets/layout_builder.dart';
+import '../widgets/thick_container.dart';
+
+class TicketView extends StatelessWidget {
+  final Map<String, dynamic> ticket;
+  final bool? isColor;
+
+  const TicketView({Key? key, required this.ticket, this.isColor})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final size = AppLayout.getSize(context);
+    return SizedBox(
+      width: size.width * 0.85,
+      height: AppLayout.getHeight(GetPlatform.isAndroid == true ? 167 : 169),
+      child: Container(
+        margin: EdgeInsets.only(right: AppLayout.getHeight(16)),
+        child: Column(
+          children: [
+            /* Showing the blue part of the ticket card */
+            Container(
+              decoration: BoxDecoration(
+                  color:
+                      isColor == null ? const Color(0xFF526799) : Colors.white,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(AppLayout.getHeight(21)),
+                      topRight: Radius.circular(AppLayout.getHeight(21)))),
+              padding: EdgeInsets.all(AppLayout.getHeight(16)),
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Text(
+                        ticket['from']['code'],
+                        style: isColor == null
+                            ? Styles.headlineStyle3
+                                .copyWith(color: Colors.white)
+                            : Styles.headlineStyle3,
+                      ),
+                      const Spacer(),
+                      ThickContainer(
+                        isColor: isColor,
+                      ),
+                      Expanded(
+                          child: Stack(
+                        children: [
+                          SizedBox(
+                              height: AppLayout.getHeight(24),
+                              child: AppLayoutBuilder(
+                                sections: 6,
+                                isColor: isColor,
+                              )),
+                          Center(
+                            child: Transform.rotate(
+                              angle: 1.5,
+                              child: Icon(
+                                Icons.local_airport_rounded,
+                                color: isColor == null
+                                    ? Colors.white
+                                    : const Color(0xFF8ACCF7),
+                              ),
+                            ),
+                          ),
+                        ],
+                      )),
+                      ThickContainer(
+                        isColor: isColor,
+                      ),
+                      const Spacer(),
+                      Text(
+                        ticket['to']['code'],
+                        style: isColor == null
+                            ? Styles.headlineStyle3
+                                .copyWith(color: Colors.white)
+                            : Styles.headlineStyle3,
+                      ),
+                    ],
+                  ),
+                  const Gap(3),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      SizedBox(
+                        width: AppLayout.getWidth(100),
+                        child: Text(
+                          ticket['from']['name'],
+                          style: isColor == null
+                              ? Styles.headlineStyle4
+                                  .copyWith(color: Colors.white)
+                              : Styles.headlineStyle4,
+                        ),
+                      ),
+                      Text(
+                        ticket['flying_time'],
+                        style: isColor == null
+                            ? Styles.headlineStyle4
+                                .copyWith(color: Colors.white)
+                            : Styles.headlineStyle4,
+                      ),
+                      SizedBox(
+                        width: AppLayout.getWidth(100),
+                        child: Text(
+                          ticket['to']['name'],
+                          textAlign: TextAlign.end,
+                          style: isColor == null
+                              ? Styles.headlineStyle4
+                                  .copyWith(color: Colors.white)
+                              : Styles.headlineStyle4,
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+            /* Showing the orange part of the ticket card */
+            Container(
+              color: isColor == null ? Styles.orangeColor : Colors.white,
+              child: Row(
+                children: [
+                  SizedBox(
+                    height: AppLayout.getHeight(20),
+                    width: AppLayout.getWidth(10),
+                    child: const DecoratedBox(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.only(
+                            topRight: Radius.circular(10),
+                            bottomRight: Radius.circular(10)),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                      child: Padding(
+                          padding: const EdgeInsets.all(12.0),
+                          child: AppLayoutBuilder(
+                            sections: 18,
+                            isColor: isColor,
+                          ))),
+                  const SizedBox(
+                    height: 20,
+                    width: 10,
+                    child: DecoratedBox(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(10),
+                            bottomLeft: Radius.circular(10)),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            /* Bottom part of the orange ticket card*/
+            Container(
+              decoration: BoxDecoration(
+                  color: isColor == null ? Styles.orangeColor : Colors.white,
+                  borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(isColor == null ? 21 : 0),
+                      bottomRight: Radius.circular(isColor == null ? 21 : 0))),
+              padding: const EdgeInsets.only(
+                  left: 16, top: 10, right: 16, bottom: 16),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      AppColumnLayout(
+                        firstText: ticket['date'],
+                        secondText: "Date",
+                        alignment: CrossAxisAlignment.start,
+                        isColor: isColor,
+                      ),
+                      AppColumnLayout(
+                        firstText: ticket['departure_time'],
+                        secondText: "Departure",
+                        alignment: CrossAxisAlignment.center,
+                        isColor: isColor,
+                      ),
+                      AppColumnLayout(
+                        firstText: '${ticket['number']}',
+                        secondText: "Number",
+                        alignment: CrossAxisAlignment.end,
+                        isColor: isColor,
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
